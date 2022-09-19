9473ad231989cb13fb379e4fd8df2d0616572173
Razor-90 <razor-90@bk.ru>
2022-09-19 20:58:09 +0600

    2

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Tag;
 
 class MainController extends Controller
-{
-    //
-
+{   
     public function index()
     {
+        $tag = new Tag();
+
+        $tag->title = 'test';
+
+        $tag->save();
+
         return view('admin.index');
     }
 }
