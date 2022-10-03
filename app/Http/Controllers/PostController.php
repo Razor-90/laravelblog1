1b2de5b8b116a648e6b3e6cfe2ae6c1f121c2aea
Razor-90 <razor-90@bk.ru>
2022-10-03 21:50:49 +0600

    ready4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,21 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Post;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    
+
     public function index()
     {
         $posts = Post::with('category')->orderBy('id', 'desc')->paginate(2);
-        return view('posts.index', compact('posts'));    
+        return view('posts.index', compact('posts'));
     }
 
     public function show($slug)
     {
-        return view('posts.show');
+        $post = Post::where('slug', $slug)->firstOrFail();
+        $post->views += 1;
+        $post->update();
+        return view('posts.show', compact('post'));
     }
-    
+
 }
